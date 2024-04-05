34144577d21dfca0350098780402747b2cad8521
2Smean <136636643+2Smean@users.noreply.github.com>
2024-04-05 18:24:47 +0900

    first commit

---
+++ src/main/java/org/example/toyone/ToyOneApplication.java
@@ -0,0 +1,13 @@
+package org.example.toyone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyOneApplication.class, args);
+	}
+
+}
